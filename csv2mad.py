1344cc840238f06ada46d87f73091d1d9341348c
Toya <omarbest.zafar@gmail.com>
2023-07-16 17:56:19 +0200

    Optimize python code

--- csv2mad.py
+++ csv2mad.py
@@ -1,20 +1,9 @@
 #!/bin/python3
 
-
-
-"""
-How to use:
-    python3 csv2mad.py ArcadeDatabase[DATE](.csv)
-
-This will generate an "output_mads/" folder with 1 file per line in your ARCADE_METADATA_FILE.
-"""
-
 import os
 import sys
+import csv
 import tqdm
-import datetime
-import numpy as np
-import pandas as pd
 
 if len(sys.argv) != 2:
     print("Please call the script as follows")
@@ -22,57 +11,43 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 ARCADE_METADATA_CSV = sys.argv[1]
-df = pd.read_csv(ARCADE_METADATA_CSV, na_values="", dtype=str)
-df = df.replace(np.nan, '', regex=True)
 MAD_NAME_COLUMN = "name"
-
-# Create output directory
 OUTPUT_DIR = "mad"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-############### MAIN LOOP #########################
-for _, game in tqdm.tqdm(df.iterrows(), desc="Generating mads", total=df.shape[0]):
+# MAIN LOOP
+with open(ARCADE_METADATA_CSV, "r") as file:
+    csv_reader = csv.DictReader(file)
+    total_rows = sum(1 for _ in csv_reader)
+    file.seek(0)  # Reset file pointer for tqdm
+    next(csv_reader)  # Skip header row
 
-    # This code block is if you want to have an alternatives MADs. Alternate mads are created in subfolders
-    # if len(game.alternative) > 0:
-    #     alternate_folder = os.path.join(OUTPUT_DIR, "_alternatives", "_"+game.alternative.replace("&amp;", "&"))
-    #     os.makedirs(alternate_folder, exist_ok=True)
-    #     mad_filename = os.path.join(alternate_folder, game[MAD_NAME_COLUMN] + ".mad")
-    # else:
-    mad_filename = os.path.join(OUTPUT_DIR, game[MAD_NAME_COLUMN] + ".mad")
+    for game in tqdm.tqdm(csv_reader, desc="Generating mads", total=total_rows):
+        mad_filename = os.path.join(OUTPUT_DIR, game[MAD_NAME_COLUMN] + ".mad")
 
-    with open(mad_filename.replace("&amp;", "&"), 'w') as f:  # Write in file as utf-8
-        f.write("<?xml version=\"1.0\" ?>\n")
-        f.write("<misterarcadedescription>\n\n")
-        for key in game.keys():
-            #if key == MAD_NAME_COLUMN:
-                # We don't want to include the MAD name as a field in the MAD itself.
-                #continue
-            #elif "linebreak" in key:
-            if "linebreak" in key:
-                # Any column that contains "linebreak" in its column name will be skipped and replaced by a
-                # new line in the MAD instead. (Remember, column names must be unique, so they will be
-                # linebreak1, linebreak2, etc.)
-                f.write("\n")
-                continue
+        with open(mad_filename.replace("&amp;", "&"), 'w') as f:
+            f.write("<?xml version=\"1.0\" ?>\n")
+            f.write("<misterarcadedescription>\n\n")
 
-            # Indent
-            f.write("\t")
+            for key, value in game.items():
+                if "linebreak" in key:
+                    f.write("\n")
+                    continue
 
-            if len(game[key]) > 0 and game[key][0] == "<":
-                # Special case, some fields contain xml code inside them (they are complete without us
-                # handling writing the xml tag around the value)
-                    f.write(game[key].replace("\\n", "\n\t\t") + "\n")
-            else:
-                if key in ["manufacturer", "series"]:
-                    for i, val in enumerate(game[key].split(" / ")):
-                        f.write("<{}>{}</{}>".format(key, val, key) + "\n")
-                        if i < len(game[key].split(" / ")) - 1:
-                            f.write("\t")
+                f.write("\t")
+
+                if len(value) > 0 and value[0] == "<":
+                    f.write(value.replace("\\n", "\n\t\t") + "\n")
                 else:
-                    f.write("<{}>{}</{}>\n".format(key, game[key], key))
+                    if key in ["manufacturer", "series"]:
+                        values = value.split(" / ")
+                        for i, val in enumerate(values):
+                            f.write(f"<{key}>{val}</{key}>\n")
+                            if i < len(values) - 1:
+                                f.write("\t")
+                    else:
+                        f.write(f"<{key}>{value}</{key}>\n")
 
-        f.write("</misterarcadedescription>")
-############### END MAIN LOOP ##########################
+            f.write("</misterarcadedescription>")
 
-print("MADs have been generated in folder {}/".format(OUTPUT_DIR))
+print(f"MADs have been generated in folder {OUTPUT_DIR}/")
